09980fc2af1aacbb5a3f859109c0cc973f4c123e
yuriy.mametskiy <yuriy.mametskiy@dukascopy.com>
2022-09-27 18:35:58 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', function(){
         // normal options...
         infinite: true,
         slidesToShow: 3,
+        variableWidth: true,
         dots: false,
         arrows: false,
         autoplay: true,
         autoplaySpeed: 5000,
+        speed: 1000,
         // centerPadding: '40px',
     });
 });
