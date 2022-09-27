f3165b287c1d2133f6d84529d4e77c98e47a4414
yuriy.mametskiy <yuriy.mametskiy@dukascopy.com>
2022-09-27 18:28:16 +0300

    add slider

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ exports.stylesMin = stylesMin;
 let scripts =()=> {
     return src([
         './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/slick-carousel/slick/slick.min.js',
         './src/js/**/*.js'
     ])
         .pipe(concat('script.js'))
---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    $(".slickSlider").slick({
+        // normal options...
+        infinite: true,
+        slidesToShow: 3,
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        // centerPadding: '40px',
+    });
+});
